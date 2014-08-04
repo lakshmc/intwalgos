8225f7251ec1cb5131b9793b919e543336c1f149
lakshmc <kanthmohan@gmail.com>
2014-08-04 13:14:12 -0700

    comment

--- src/main/java/stringarray/RebuildSentence.java
+++ src/main/java/stringarray/RebuildSentence.java
@@ -14,6 +14,7 @@ import java.util.*;
  *
  *  Input:  ilikesamsunga
  *  String cannot be segmented (since there is no 'a' in dictionary)
+ *  http://www.geeksforgeeks.org/dynamic-programming-set-32-word-break-problem/
  */
 public class RebuildSentence {
 
