c8cd04ccfaaea0ed15f4131642248b6a31ada63c
lakshmc <kanthmohan@gmail.com>
2015-01-22 10:42:33 -0700

    comments

--- src/main/java/stringarray/RebuildSentence.java
+++ src/main/java/stringarray/RebuildSentence.java
@@ -15,6 +15,18 @@ import java.util.*;
  *  Input:  ilikesamsunga
  *  String cannot be segmented (since there is no 'a' in dictionary)
  *  http://www.geeksforgeeks.org/dynamic-programming-set-32-word-break-problem/
+ *  Assumption: Input is cannot partially right, i.e., in the third example above,
+ *  the input is invalid since the final a does not form a proper word, even though rest of the input resolves to
+ *  proper words.
+ *
+ *  Solution: Iterate recursively through the given string. The solution can be seen as this:
+ *  - first word of the sentence + <space> + rest of the words in the sentence
+ *  - so take a prefix and check if its a word
+ *       - if so check if the rest of the sentence also has proper words
+ *          - if true, then the prefix word and suffix words are valid and print the result
+ *          - if false, then the current prefix is not proper (See assumption). Attach another letter to the prefix
+ *          and repeat to see if this prefix combination results in a valid suffix.
+ *
  */
 public class RebuildSentence {
 
@@ -22,14 +34,15 @@ public class RebuildSentence {
         Set<String> dict = new HashSet<String>(Arrays.asList("mobile","samsung","sam","sung","man","mango",
                                                             "icecream","and","go","i","like","ice","cream"));
 
-        System.out.println(wordBreak("ilikemangoicecream", dict));
-        System.out.println(wordBreak("ilikesamsungis", dict));
+        System.out.println(wordBreakBoolean("ilikemangoicecream", dict));
+        System.out.println(wordBreakBoolean("ilikesamsungis", dict));
 
-        wordBreak("ilikemangoicecream", dict,"");
+        wordBreakAddToList("ilikemangoicecream", dict,"");
+        wordBreakAllValidCombinations("ice", dict, "");
+        System.out.println(wordBreakBoolean("ilikesamsungis", dict));
 
-        List<String> resultList = new ArrayList<String>();
-        wordBreak("ilikemangoicecream", dict,resultList,"");
-        System.out.println(resultList);
+        wordBreakAllValidCombinations("ilikemangoicecream", dict, "");
+        wordBreakAllValidCombinations("ice", dict, "");
 
     }
 
@@ -37,7 +50,7 @@ public class RebuildSentence {
      * prints true/false based on whether a sentence can be built from the input
      * @return
      */
-    private static boolean wordBreak(String str, Set<String> dict){
+    private static boolean wordBreakBoolean(String str, Set<String> dict){
         int len = str.length();
         if (len==0)
             return true;
@@ -45,8 +58,8 @@ public class RebuildSentence {
         for(int i=1;i<=len;i++){
             String prefix = str.substring(0,i);
             String suffix = str.substring(i,len);
-            if(dict.contains(prefix) && wordBreak(suffix, dict)){
-                System.out.println(prefix);
+            if(dict.contains(prefix) && wordBreakBoolean(suffix, dict)){
+                System.out.println(prefix); // prints as soon as a word is detected
                 return true;
             }
         }
@@ -56,7 +69,7 @@ public class RebuildSentence {
     /**
      * prints all the valid combination of sentences that can be built from the input.
      */
-    private static void wordBreak(String str, Set<String> dict, String result){
+    private static void wordBreakAllValidCombinations(String str, Set<String> dict, String result){
         int len = str.length();
         for(int i=1;i<=len;i++){
             String prefix = str.substring(0,i);
@@ -68,32 +81,27 @@ public class RebuildSentence {
                     System.out.println(result);
                     return;
                 }
-                wordBreak(suffix, dict, result+prefix+" ");
+                wordBreakAllValidCombinations(suffix, dict, result + prefix + " ");
             }
         }
     }
 
     /**
-     * Returns true if a valid sentence can be formed from the given input. The combinations are added into
-     * resultList
+     * Same as above, slightly different logic
      */
-    private static boolean wordBreak(String str, Set<String> dict, List<String> resultList, String addWord){
-        String prefix="";
-        boolean isTrue= false;
-        if(str.length()==0) {
-            resultList.add(addWord);
-            return true;
+    private static void wordBreakAddToList(String str, Set<String> dict, String sentence){
+        String prefix="", suffix="";
+        if(str.length()==0) { // we have parsed all the characters for this recursion
+            System.out.println(sentence);
         }
 
-        for(int i=1; i<=str.length();i++) {
+        for(int i=1; i<=str.length();i++) { // start from 1 (not 0) since we are using substring
             prefix=str.substring(0, i);
+            suffix=str.substring(i);
             if(dict.contains(prefix)) {
-                // using substring(beginIndex)
-                wordBreak(str.substring(i), dict, resultList, addWord + " " + prefix);
-                isTrue = true;
+                wordBreakAddToList(suffix, dict, sentence + prefix + " ");
             }
         }
-        return isTrue;
 
     }
 }
