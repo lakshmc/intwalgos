fdc549ce99a5a850398b1455f5eb2af80983939b
lakshmc <kanthmohan@gmail.com>
2015-02-12 09:29:53 -0700

    Remove duplicate strings from an array, print a string array

---
+++ src/main/java/stringarray/RemoveDuplicateString.java
@@ -0,0 +1,84 @@
+package stringarray;
+
+import util.Printer;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an array of string, remove duplicates
+ * 1. Iterate and load each item into a set and then load it into an new array with set's size
+ * - uses extra space for set and order of items get changed since data is loaded from set.
+ * - O(n)
+ *
+ * 2. removeDuplicatesSetWithOrder(): Use set to identify unique strings, and mark the duplicates
+ * with "". then iterate the array and move the empty ones to the end using double index
+ * - O(n)
+ *
+ * 3. Sort it and load the first occurrence of each string into a new array. Doesnt keep order, uses
+ * additional memory
+ * - O(nlog(n))
+ */
+public class RemoveDuplicateString {
+
+    public static void main(String[] args) {
+
+        removeDuplicatesSetNoOrder(new String[]{"ab", "ab", "abs", "abcd", "adas", "abcd", "ab", "abs", "adasf"});
+        removeDuplicatesSetWithOrder(new String[]{"ab","ab","abs","abcd","adas","abcd","ab","abs","adasf"});
+        removeDuplicatesSortWithOrder(new String[]{"ab", "ab", "abs", "abcd", "adas", "abcd", "ab", "abs", "adasf"});
+    }
+
+    private static void removeDuplicatesSetNoOrder(String[] strings) {
+        /*System.out.println("Before");
+        Printer.printStrArray(strings);*/
+        Set<String> set = new HashSet<String>();
+        for (int i=0;i<strings.length;i++) {
+            if(!set.add(strings[i]))
+                strings[i] = "";
+        }
+        String[] uStr = new String[set.size()];
+        int i=0;
+        for(String str: set){
+            uStr[i++]=str;
+        }
+        System.out.println("After");
+        Printer.printStrArray(uStr);
+    }
+
+    private static void removeDuplicatesSetWithOrder(String[] strings) {
+        Set<String> set = new HashSet<String>();
+        for (int i=0;i<strings.length;i++) {
+            if(!set.add(strings[i]))
+                strings[i] = "";
+        }
+        int uniqueIndex=0;
+        if(set.size()<strings.length){
+            for(int i=0;i<strings.length;i++){
+                if(!strings[i].equals("") ){
+                    if(uniqueIndex!=i){
+                        strings[uniqueIndex++]=strings[i];
+                        strings[i]="";
+                    } else {
+                        uniqueIndex++;
+                    }
+                }
+            }
+        }
+        Printer.printStrArray(strings);
+    }
+
+    private static void removeDuplicatesSortWithOrder(String[] strings) {
+        Arrays.sort(strings);
+        String[] newDuplicates = new String[strings.length];
+        int k=0;
+        for (int i=0;i<strings.length-1;i++) {
+            if(strings[i].equals(strings[i+1])){
+                continue;
+            }
+            newDuplicates[k++]=strings[i];
+        }
+        System.out.println("After");
+        Printer.printStrArray(newDuplicates);
+    }
+}
--- src/main/java/util/Printer.java
+++ src/main/java/util/Printer.java
@@ -14,4 +14,15 @@ public class Printer {
         }
         System.out.print("]\n");
     }
+
+    public static void printStrArray(String[] arr){
+        System.out.print("\n[");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+            if(i<arr.length-1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("]\n");
+    }
 }
